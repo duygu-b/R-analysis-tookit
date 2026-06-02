e703dead70490a1a72b57fd86b0867d18d54cf1c
duygu-b <duygubuga91@gmail.com>
2026-06-02 12:09:20 +0100

    Update bilingual_anova_power.R

--- bilingual_anova_power.R
+++ bilingual_anova_power.R
@@ -2,17 +2,30 @@ install.packages("Superpower")
 library(Superpower)
 
 design_result <- ANOVA_design(
-  design = "3b*4w",              # 3 groups (between) x 4 Stroop types (within)
-  n = 45,                        # number of participants per group
-  mu = c(2800, 2500, 3000, 2700,   # Turkish-English bilingual: Congruent, Incongruent
-         2850, 2550, 2950, 2750,   # Spanish-English bilingual
-         3000, 2900, 3100, 2950),  # Monolingual
+  design = "3b*2w*2w",              # 3 grup (between) x 2 Stroop type (within) x 2 Sentence type (within)
+  n = 45,                     
+  mu = c( #Cong vs Incong
+          # Turkish-English
+          3409, 3765, #amb
+          3132, 2861, #unamb
+          # Spanish-English
+          4336, 4474, 
+          3586, 3373,
+          # Monolingual
+          4234, 4124, 
+          3164, 3157),
   sd = 300,                      # reasonable standard deviation for RT
   r = 0.5,                       # correlation between congruent and incongruent conditions
-  labelnames = c("Group", "TurkEng", "SpanEng", "MonoEng",
-                 "Condition", "Cong-amb", "Incong-amb", "Cong-unamb", "Incong-unamb")
+  labelnames = c(
+    "Group", "TurkEng", "SpanEng", "MonoEng",
+    "Sentence", "Amb", "Unamb", 
+    "Stroop", "Cong", "Incong")
 )
 
-power_result <- ANOVA_power( design_result, alpha_level = 0.05, nsims = 1000 )
+power_result <- ANOVA_power(
+  design_result,
+  alpha_level = 0.05,
+  nsims = 1000
+)
 
 print(power_result)
