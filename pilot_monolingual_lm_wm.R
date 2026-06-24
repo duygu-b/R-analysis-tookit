c0584e6af5a4d85958d8eb18b15507bf5577f885
duygu-b <duygubuga91@gmail.com>
2026-06-24 12:16:11 +0100

    Update pilot_monolingual_lm_wm.R

--- pilot_monolingual_lm_wm.R
+++ pilot_monolingual_lm_wm.R
@@ -3,12 +3,12 @@ library(readxl)
 df <- read.csv("documents/all.csv") # Loading dataset (pilot monolingual data)
 
 df$logRT <- log(df$CriticalRegionRT) # Log-transform reaction times to reduce skewness in RT distribution
-df$sentence_type <- factor(df$sentence_type) # Converting categorical predictors into factors for modeling
+df$condition <- factor(df$condition) # Converting categorical predictors into factors for modeling
 
 # Fitting linear model (no random effects; pilot analysis only)
 # Testing main effects and interaction of sentence type and Stroop condition
 df$previous_stroop <- factor(df$previous_stroop)
 
-model <- lm(logRT ~ sentence_type * previous_stroop, data = df)
+model <- lm(logRT ~ condition * previous_stroop, data = df)
 
 summary(model)
